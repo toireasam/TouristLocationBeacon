aa3a3f10ad568c8cf26a89183e850e16125ae7c0
Toireasa Moley <toireasam@Hoxton.home>
2015-11-15 19:37:34 +0000

    Parse class added

---
+++ Parse.h
@@ -0,0 +1,13 @@
+//
+//  Parse.h
+//  Example
+//
+//  Created by Toireasa Moley on 15/11/2015.
+//  Copyright © 2015 jaalee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parse : NSObject
+- (void)TakeID;
+@end
